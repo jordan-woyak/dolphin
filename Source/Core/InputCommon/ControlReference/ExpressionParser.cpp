4646b2f726a6a77967c8bcbfc2b8144120d13d5f
Jordan Woyak <jordan.woyak@gmail.com>
2018-12-30 16:11:42 -0600

    ExpressionParser: Add less-than and greater-than operators.

--- Source/Core/InputCommon/ControlReference/ExpressionParser.cpp
+++ Source/Core/InputCommon/ControlReference/ExpressionParser.cpp
@@ -38,6 +38,8 @@ enum TokenType
   TOK_DIV,
   TOK_MOD,
   TOK_ASSIGN,
+  TOK_LTHAN,
+  TOK_GTHAN,
   TOK_CONTROL,
   TOK_LITERAL,
   TOK_VARIABLE,
@@ -63,6 +65,10 @@ inline std::string OpName(TokenType op)
     return "Mod";
   case TOK_ASSIGN:
     return "Assign";
+  case TOK_LTHAN:
+    return "LThan";
+  case TOK_GTHAN:
+    return "GThan";
   case TOK_VARIABLE:
     return "Var";
   default:
@@ -107,6 +113,10 @@ public:
       return "%";
     case TOK_ASSIGN:
       return "=";
+    case TOK_LTHAN:
+      return "<";
+    case TOK_GTHAN:
+      return ">";
     case TOK_CONTROL:
       return "Device(" + data + ")";
     case TOK_LITERAL:
@@ -228,6 +238,10 @@ public:
       return Token(TOK_MOD);
     case '=':
       return Token(TOK_ASSIGN);
+    case '<':
+      return Token(TOK_LTHAN);
+    case '>':
+      return Token(TOK_GTHAN);
     case '\'':
       return GetLiteral();
     case '$':
@@ -338,6 +352,10 @@ public:
       // TODO: Should this instead GetValue(lhs) ?
       return rhsValue;
     }
+    case TOK_LTHAN:
+      return lhsValue < rhsValue;
+    case TOK_GTHAN:
+      return lhsValue > rhsValue;
     default:
       assert(false);
       return 0;
@@ -727,6 +745,8 @@ private:
     case TOK_DIV:
     case TOK_MOD:
     case TOK_ASSIGN:
+    case TOK_LTHAN:
+    case TOK_GTHAN:
       return true;
     default:
       return false;
