bbe00e40b26f8583300e9d40031d783df2b205a0
Jordan Woyak <jordan.woyak@gmail.com>
2025-11-16 17:17:22 -0600

    Core: Make RunOnCPUThread's wait_for_completion parameter default to false in preparation for removing it entirely.

--- Source/Core/Core/Core.h
+++ Source/Core/Core/Core.h
@@ -160,7 +160,7 @@ void OnFrameEnd(Core::System& system);
 
 // Run a function on the CPU thread, asynchronously.
 void RunOnCPUThread(Core::System& system, Common::MoveOnlyFunction<void()> function,
-                    bool wait_for_completion);
+                    bool wait_for_completion = false);
 
 // for calling back into UI code without introducing a dependency on it in core
 using StateChangedCallbackFunc = std::function<void(Core::State)>;
