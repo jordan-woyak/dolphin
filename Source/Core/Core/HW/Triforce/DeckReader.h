bd131949eefce61c119a42f2b66fdce8a5f664de
Jordan Woyak <jordan.woyak@gmail.com>
2026-03-17 21:46:52 -0500

    Triforce: ICCardReader and DeckReader improvements.
    Significantly rewrote ICCardReader and DeckReader functionality.
    Dual IC card slots of VirtuaStriker4 + Gekitou now work.
    IC cards are automatically inserted/ejected in VirtuaStriker4 + Gekitou + Avalon.
    IC card data is saved to a file.
    Avalon deck contents are loaded from a JSON file.
    Added IOPorts class to handle bespoke GPIO functionality.

---
+++ Source/Core/Core/HW/Triforce/DeckReader.h
@@ -0,0 +1,40 @@
+// Copyright 2026 Dolphin Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "Common/DirectIOFile.h"
+
+#include "Core/HW/Triforce/ICCardReader.h"
+#include "Core/HW/Triforce/SerialDevice.h"
+
+class PointerWrap;
+
+namespace Triforce
+{
+
+// Serial deck reader used by The Key of Avalon games.
+class DeckReader final : public SerialDevice
+{
+public:
+  void Update() override;
+
+  void DoState(PointerWrap& p) override;
+
+  auto* GetICCardReader() { return &m_ic_card_reader; }
+
+private:
+  // It seems that the IC Card Reader must be connected through the Deck Reader.
+  // The Deck Reader forwards appropriate commands to the IC Card Reader.
+  // IC Card Reader responses are passed through back to the baseboard as-is.
+  // It can't be the other way around because Deck Reader commands are unmarked variable lengths.
+  ICCardReader m_ic_card_reader{0};
+
+  void HandleFirmwareUpdate();
+
+  u8 m_firmware_update_timeout = 0;
+
+  File::DirectIOFile m_firmware_dump_file;
+};
+
+}  // namespace Triforce
