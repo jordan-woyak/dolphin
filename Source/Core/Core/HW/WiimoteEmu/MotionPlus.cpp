fde5651ff997673c5cdb79630653c8a72db33ae6
Jordan Woyak <jordan.woyak@gmail.com>
2026-04-15 01:38:12 -0500

    HW/WiimoteEmu/MotionPlus: Hardcode the challenge responses to not depend on mbedtls.

--- Source/Core/Core/HW/WiimoteEmu/MotionPlus.cpp
+++ Source/Core/Core/HW/WiimoteEmu/MotionPlus.cpp
@@ -5,9 +5,6 @@
 
 #include <algorithm>
 #include <cmath>
-#include <iterator>
-
-#include <mbedtls/bignum.h>
 
 #include "Common/BitUtils.h"
 #include "Common/ChunkFile.h"
@@ -17,39 +14,6 @@
 
 #include "Core/HW/Wiimote.h"
 #include "Core/HW/WiimoteEmu/Extension/DesiredExtensionState.h"
-
-namespace
-{
-// Minimal wrapper mainly to handle init/free
-struct MPI : mbedtls_mpi
-{
-  explicit MPI(const char* base_10_str) : MPI() { mbedtls_mpi_read_string(this, 10, base_10_str); }
-
-  MPI() { mbedtls_mpi_init(this); }
-  ~MPI() { mbedtls_mpi_free(this); }
-
-  mbedtls_mpi* Data() { return this; }
-
-  template <std::size_t N>
-  bool ReadBinary(const u8 (&in_data)[N])
-  {
-    return 0 == mbedtls_mpi_read_binary(this, std::begin(in_data), std::size(in_data));
-  }
-
-  template <std::size_t N>
-  bool WriteLittleEndianBinary(std::array<u8, N>* out_data)
-  {
-    if (mbedtls_mpi_write_binary(this, out_data->data(), out_data->size()))
-      return false;
-
-    std::ranges::reverse(*out_data);
-    return true;
-  }
-
-  MPI(const MPI&) = delete;
-  MPI& operator=(const MPI&) = delete;
-};
-}  // namespace
 
 namespace WiimoteEmu
 {
@@ -466,30 +430,19 @@ void MotionPlus::Update(const DesiredExtensionState& target_state)
   if (m_progress_timer)
     return;
 
-  // This is potentially any value that is less than cert_n and >= 2.
-  // A real M+ uses random values each run.
-  constexpr u8 magic[] = "DOLPHIN DOES WHAT NINTENDON'T.";
-
-  constexpr char cert_n[] =
-      "67614561104116375676885818084175632651294951727285593632649596941616763967271774525888270484"
-      "88546653264235848263182009106217734439508352645687684489830161";
-
-  constexpr char sqrt_v[] =
-      "22331959796794118515742337844101477131884013381589363004659408068948154670914705521646304758"
-      "02483462872732436570235909421331424649287229820640697259759264";
-
   switch (m_reg_data.challenge_state)
   {
   case ChallengeState::PreparingX:
   {
-    MPI param_x;
-    param_x.ReadBinary(magic);
+    constexpr u8 param_x[64] = {
+        0x00, 0x00, 0x44, 0x38, 0xb2, 0xc9, 0xba, 0x61, 0xe2, 0x50, 0x9e, 0x53, 0x06,
+        0xbe, 0xa5, 0x35, 0x0a, 0x81, 0xf0, 0x53, 0x7e, 0x70, 0x27, 0x62, 0xc5, 0xdf,
+        0xbe, 0x52, 0x13, 0x22, 0x72, 0xa3, 0x28, 0x25, 0x1b, 0x7c, 0xb5, 0x48, 0xe4,
+        0xd5, 0x37, 0x45, 0x98, 0xc4, 0x5a, 0x16, 0x4f, 0x3d, 0xdc, 0x81, 0x6a, 0x93,
+        0xd1, 0x24, 0xd8, 0x52, 0xb3, 0xd6, 0x1a, 0x39, 0x3a, 0x12, 0x00, 0x00,
+    };
 
-    mbedtls_mpi_mul_mpi(&param_x, &param_x, &param_x);
-    mbedtls_mpi_mod_mpi(&param_x, &param_x, MPI(cert_n).Data());
-
-    // Big-int little endian parameter x.
-    param_x.WriteLittleEndianBinary(&m_reg_data.challenge_data);
+    std::ranges::copy(param_x, m_reg_data.challenge_data.data());
 
     DEBUG_LOG_FMT(WIIMOTE, "M+ parameter x ready.");
     m_reg_data.challenge_state = ChallengeState::ParameterXReady;
@@ -499,22 +452,28 @@ void MotionPlus::Update(const DesiredExtensionState& target_state)
   case ChallengeState::PreparingY:
     if (0 == m_reg_data.challenge_type)
     {
-      MPI param_y0;
-      param_y0.ReadBinary(magic);
+      // "DOLPHIN DOES WHAT NINTENDON'T."
+      constexpr u8 param_y0[64] = {
+          0x00, 0x2e, 0x54, 0x27, 0x4e, 0x4f, 0x44, 0x4e, 0x45, 0x54, 0x4e, 0x49, 0x4e,
+          0x20, 0x54, 0x41, 0x48, 0x57, 0x20, 0x53, 0x45, 0x4f, 0x44, 0x20, 0x4e, 0x49,
+          0x48, 0x50, 0x4c, 0x4f, 0x44, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+          0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+          0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      };
 
-      // Big-int little endian parameter y0.
-      param_y0.WriteLittleEndianBinary(&m_reg_data.challenge_data);
+      std::ranges::copy(param_y0, m_reg_data.challenge_data.data());
     }
     else
     {
-      MPI param_y1;
-      param_y1.ReadBinary(magic);
+      constexpr u8 param_y1[64] = {
+          0xaa, 0x4b, 0xba, 0xfd, 0xe9, 0xfe, 0xff, 0x8a, 0x91, 0x0f, 0xa1, 0xec, 0x60,
+          0xc6, 0x5c, 0xf8, 0x46, 0x59, 0xe9, 0xa0, 0xc2, 0x73, 0xbd, 0x7f, 0xcf, 0x7c,
+          0xd6, 0x39, 0x32, 0x70, 0x45, 0x00, 0x99, 0x4f, 0x87, 0xe0, 0x22, 0xfc, 0xce,
+          0xf8, 0x8a, 0xbb, 0x87, 0x34, 0xd5, 0xc8, 0x08, 0x69, 0xe1, 0xa9, 0x27, 0x2f,
+          0xef, 0x81, 0xde, 0x66, 0xac, 0x33, 0x68, 0x1d, 0x58, 0xc3, 0x44, 0x2e,
+      };
 
-      mbedtls_mpi_mul_mpi(&param_y1, &param_y1, MPI(sqrt_v).Data());
-      mbedtls_mpi_mod_mpi(&param_y1, &param_y1, MPI(cert_n).Data());
-
-      // Big-int little endian parameter y1.
-      param_y1.WriteLittleEndianBinary(&m_reg_data.challenge_data);
+      std::ranges::copy(param_y1, m_reg_data.challenge_data.data());
     }
 
     DEBUG_LOG_FMT(WIIMOTE, "M+ parameter y ready.");
